37bfe8c9e41031ad545bf5fcca2cb46d4e304eb2
BruceWyne2001 <1520182685@qq.com>
2023-11-09 08:37:48 +0800

    Initial commit

---
+++ cc.py
@@ -0,0 +1,58 @@
+# print('丘吉尔说：“成功就是失败到失败，也依然不改热情！”')
+#
+# print("My favorite sports are as follows:\n\tfootball\n\trable tennis\n\tswimming\n running")
+#
+# print("my favorite sports are as follows:")
+# print(str("\tfootball\n\t rable tennis\n\tswimmingn\n ").upper())
+#
+# print("%s\t%.1f\t%d\t%.2f" % ("dora", 899, 10, 899 / 10))
+#
+# # chickenrabbit.py
+# iHeads = 35
+# iFeet = 94
+# a = iFeet - 2 * iHeads
+# iRabbits = a / 2
+# iChicken = iHeads - iRabbits
+# print("Number of chicken=%d," % iChicken, "Numbre of rabbits=%d" % iRabbits)
+# print(iFeet == iChicken * 2 + iRabbits * 4)
+#
+# # # input.py
+# # a = int(input("how C it is:"))
+# # print("c=", 5 / 9 * (a - 32))
+#
+# b = int(input("num1:"))
+# c = int(input("num2:"))
+# print("num=", b + c)
+# print("n=", (b + c) / 2)
+#
+# r = int(input("r="))
+# print("C=", 3.14 * r * 2)
+# print("S=", 3.14 * r * r)
+#
+# spam = ['aaa', 'sss', 'ddd', 'qqq', 'www', 'eee', 'fff', 'ggg']
+# for i in range(len(spam)):
+#     print('第' + str(i) + '个元素是：' + spam[i])
+# print()
+# print(spam.index('ddd'))
+# print()
+# for index, elem in enumerate(spam):
+#     print('第' + str(index) + '个元素是' + elem)
+# print()
+# print(list(enumerate(spam)))
+#
+# lists = [1, 1, 2, 3, 4, 6, 9, 6, 2, 2]
+# lists.sort()
+# t = lists[-1]
+#
+# for i in range(len(lists) - 2, -1, -1):
+#     if t == lists[i]:
+#         lists.remove(lists[i])
+#     else:
+#         t = lists[i]
+#         print(lists[i])
+# import numpy as np
+#
+# lists = [1, 1, 2, 3, 4, 6, 9, 6, 2, 2]
+# lists = np.unique(lists)
+# print(list[i])
+
