473f66f8e0e7dfed2de87cb731e67351ace7f64d
BruceWyne <1520182685@qq.com>
2023-11-15 12:27:38 +0800

    Initial commit

--- 1.py
+++ 1.py
@@ -8,5 +8,11 @@ name7="地承坤"
 name8="君子以德载物"
 print(name1,sep="。")
 print(name4)
-print(name5,name6,name7,name8,sep=",",end="。")
+print(name5,name6,name7,name8,sep=",",end="。"'\n')
 
+#格式化输出
+name='A'
+age=18
+pho=132943245
+ten='星球'
+print('你是:%s,年龄:%d,电话号码:%d,来自于:%s' %(name,age,pho,ten),end='。')
