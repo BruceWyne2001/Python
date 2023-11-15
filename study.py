8dfc051a29b98d4bbecb566e28a0a08310ac6e4e
BruceWyne2001 <102731485+brucewyne2001@users.noreply.github.com>
2023-11-15 15:51:17 +0800

    Update python.md

---
+++ study.py
@@ -0,0 +1,33 @@
+# name1='源氏说:"尝尝神龙之剑"'
+# name2='半藏说："愿神龙指引我的箭"'
+# name3="命运的齿轮在转动"
+# name4="龙啊~请吞噬我的敌人"
+# name5="天行健"
+# name6="君子以自强不息"
+# name7="地承坤"
+# name8="君子以德载物"
+# print(name1,sep="。")
+# print(name4)
+# print(name5,name6,name7,name8,sep=",",end="。"'\n')
+#
+# #格式化输出
+# name='A'
+# age=18
+# pho=132943245
+# ten='星球'
+# print('你是:%s,年龄:%d,电话号码:%d,来自于:%s' %(name,age,pho,ten),end='\n')
+# print('先知曾经说过:{}，半藏曾经说过:{}，还有一个人{}'.format(name3,name4,name1))
+#
+# role=input("请输入的你的名字:")
+# age=input("请输入你的年龄:")
+# high=input("请输入你的身高:")
+# weight=input("请输入你的体重:")
+# shop=input("请输入你想购买的商品:")
+
+# name='abc'
+# name1=name
+# name2=name1
+# name3=name2
+# print("name3=",name)
+
+
